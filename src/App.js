5c378e5a1d52b9d9ac44455f0034559ef3177b29
dev-rahim <rahimuddin5915@gmail.com>
2023-01-29 00:01:43 +0600

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import './App.css';
 
+
 function App() {
+  const [message, setMessage] = useState('')
+  const commands = [
+    {
+      command: ['Hello', 'Hi'],
+      callback: ({ command }) => setMessage(`Hi there! You said: "${command}"`),
+      matchInterim: true
+    },
+    {
+      command: 'change color to *',
+      callback: (color) => { document.getElementById('content').style.color = { color } }
+    },
+    {
+      command: 'decrease text size',
+      callback: () => { document.getElementById('content').style.fontSize = '16px' }
+    },
+    {
+      command: 'increase text size',
+      callback: () => { document.getElementById('content').style.fontSize = '22px' }
+    },
+    {
+      command: 'open *',
+      callback: (site) => window.open('http://' + site)
+    },
+    {
+      command: 'reset',
+      callback: ({ resetTranscript }) => resetTranscript()
+    },
+    {
+      command: 'clear',
+      callback: ({ resetTranscript }) => resetTranscript()
+    }
+  ];
+  SpeechRecognition.startListening({ continuous: true, language: 'en-IN' })
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition({ commands })
+
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="nav">
+        <h2>Please speak something To write</h2>
+      </div>
+      <div id="content">
+        {transcript}
+      </div>
     </div>
   );
 }
